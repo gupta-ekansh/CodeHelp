5ce39621f1d5126a71e9e8c192b234dbd6d670af
Ekansh Gupta <ekanshgupta2019@gmail.com>
2023-01-28 15:00:42 +0530

    writing our first program

---
+++ 28th jan 2023/namaste.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    int b = 3;
+    cout << (a>=5 && b<=3) << endl;
+
+    // int a=5;
+    // int b=3;
+    // cout << (a>b) << endl;
+
+    // cout << (5.0)/3 << endl;
+    // cout << 5/(3.0) << endl;
+    // cout << 5.0/3.0 << endl;
+
+    // // implicit type casting
+    // char ch = 97;
+    // cout << ch << endl;
+
+    // int a = 'b';
+    // cout << a << endl;
+
+    // // explicit type casting
+    // double d = 5.7;
+    // int x = (int)d + 2;
+    // cout << x << endl;
+
+    // char c = 261;
+    // cout << c << endl;
+
+    // long a = 5;
+    // cout << sizeof(a) << endl;
+
+    // cout << "Namaste Bharat" << endl;
+
+    // int number = 0;
+    // cout << "Enter a number: ";
+    // cin >> number ;
+    // cout << "The entered number is: " << number << endl;
+
+    // int a = 5;
+    // cout << "The value of a is: " << a << endl;
+
+    // char ch = 'a';
+    // cout << "The value of ch is: " << ch << endl;
+
+    // bool flag = true;
+    // cout << "The value of flag is: " << flag << endl;
+
+    // char temp = 256;
+    // cout << "The value of temp is: " << temp << endl;
+}
